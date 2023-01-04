b991b6b510773188e3643bdf3721bd41db279f58
Ghaffar Hadista Hariadi <94441683+ghaffarhadista@users.noreply.github.com>
2023-01-04 14:07:17 +0700

    update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="container mt-5 pt-5">
+<div class="container mt-5 pt-5 mb-4">
     <h1 class="text-center text-dark">Tentang Kami</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illum fuga esse dolor consequatur cum qui tenetur. Enim, asperiores laborum modi illum, tempora eligendi, rerum accusamus nemo voluptatibus odit suscipit aliquid autem quo saepe vel eum qui id reiciendis quos blanditiis explicabo consectetur? Maxime fuga laborum aliquid eligendi? Velit quibusdam veritatis eaque laboriosam reprehenderit tempore porro ipsa, voluptatum natus atque voluptates? Veniam corrupti ipsam quam in totam exercitationem fugit reprehenderit aliquam veritatis facilis? Nam repudiandae cumque eaque quae provident, aut praesentium illum odio molestias eos nemo error sed obcaecati voluptates temporibus! Officiis provident maxime nostrum animi possimus, quasi sequi nobis.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illum fuga esse dolor consequatur cum qui tenetur. Enim, asperiores laborum modi illum, tempora eligendi, rerum accusamus nemo voluptatibus odit suscipit aliquid autem quo saepe vel eum qui id reiciendis quos blanditiis explicabo consectetur? Maxime fuga laborum aliquid eligendi? Velit quibusdam veritatis eaque laboriosam reprehenderit tempore porro ipsa, voluptatum natus atque voluptates? Veniam corrupti ipsam quam in totam exercitationem fugit reprehenderit aliquam veritatis facilis? Nam repudiandae cumque eaque quae provident, aut praesentium illum odio molestias eos nemo error sed obcaecati voluptates temporibus! Officiis provident maxime nostrum animi possimus, quasi sequi nobis.</p>
-</div>
+   <p>Hi! Kami G-Shoes adalah penjual Sepatu Vans HEHEHE</p>
+    <p>Web Ini bertujuan untuk menyelesaikan tugas akhir bootcamp yang saya ikuti! <i class="bi bi-arrow-through-heart-fill"></i></p>
+   </div>
 
 
 @endsection
